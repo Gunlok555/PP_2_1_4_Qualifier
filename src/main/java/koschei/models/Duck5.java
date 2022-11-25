88f6c5c8e30fa34ce5b7299f2bee0ac5367593a3
Gunlok555 <a_v30@mail.ru>
2022-11-25 11:11:40 +0400

    сделал с @Qualifier

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Component;
 public class Duck5 {
     private final Egg6 egg;
 
-
-    public Duck5(Egg6 egg) {
+    @Autowired
+    public Duck5(@Qualifier("egg6") Egg6 egg) {
         this.egg = egg;
     }
 
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
-//private final Deth8 deth;
-
-  //    public Needle7(Deth8 deth) {
-    //    this.deth = deth;
-    //}
+private final Deth8 deth;
+    @Autowired
+      public Needle7(@Qualifier("deth8") Deth8 deth) {
+        this.deth = deth;
+    }
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
